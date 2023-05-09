751200a0813a0063470d74ed0789de48757ae60f
Hieronimus Ferry <ferryhieronimus.com>
2023-05-09 21:05:35 +0700

    Update UI

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:8080/api/v1/auth/login";
+const baseUrl = "http://34.126.181.230:80/api/v1/auth/login";
 
 const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials);
