922234a59c079a6527b5d66c05825ee054ca669d
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2023-05-25 17:01:44 +0700

    change user pinjam layout

--- src/components/Pinjam/UserPinjam.jsx
+++ src/components/Pinjam/UserPinjam.jsx
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import PinjamFooter from './PinjamFooter';
 import PinjamHeader from './PinjamHeader';
 import UserListComponent from './UserListComponent';
+import Navbar from '../Headers/Navbar';
 
 class Pinjam extends Component {
     render() {
         return (
             <div>
-                <PinjamHeader/>
+                <Navbar/>
                 <div className='container'>
                     <UserListComponent/>
-                </div>
-                <div className='footer'>
-                    <PinjamFooter/>
                 </div>
             </div>
         );
